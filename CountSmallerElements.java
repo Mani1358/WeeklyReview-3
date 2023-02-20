83a9bd23c7da0dfad2582a5d022ca27e93f4532a
Mani1358 <mani621998@gmail.com>
2023-02-20 09:09:26 +0530

    Added:WeeklyReview-3 programs

--- CountSmallerElements.java
+++ CountSmallerElements.java
@@ -1,26 +1,19 @@
 package com.blz.weeklyreview3;
 
 public class CountSmallerElements {
-    static int countSmallerElements(int[] a, int numb) {
-        int l = 0, h = a.length - 1, mid = 0;
-        while (l < h) {
-            mid = (l + h) / 2;
-            if (numb == a[mid]) {
+    static int countSmallerElements(int[] arr, int n, int numb) {
+        int i = 0;
+        for (i = 0; i < n; i++) {
+            if (arr[i] > numb)
                 break;
-
-            } else if (numb < a[mid]) {
-                h = mid - 1;
-
-            } else {
-                l = mid + 1;
-            }
         }
-        return mid + 1;
+        return i;
     }
 
     public static void main(String[] args) {
         int arr[] = {2, 6, 4, 5, 12, 18, 30, 26};
         int num = 25;
-        System.out.println(countSmallerElements(arr, num));
+        int n = arr.length;
+        System.out.println(countSmallerElements(arr, n, num));
     }
 }
