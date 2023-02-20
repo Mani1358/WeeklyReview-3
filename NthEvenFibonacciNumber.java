98dc64637778d2541180b8f265de49a0728544ad
Mani1358 <116184023+Mani1358@users.noreply.github.com>
2023-02-20 09:33:18 +0530

    Update NthEvenFibonacciNumber.java

--- NthEvenFibonacciNumber.java
+++ NthEvenFibonacciNumber.java
@@ -2,7 +2,7 @@ package com.blz.weeklyreview3;
 
 public class NthEvenFibonacciNumber {
     public static void main(String[] args) {
-        int n = 3;
+        int n = 2;
         System.out.println(evenFib(n));
     }
 
